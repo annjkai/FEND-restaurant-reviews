9f9a56e86649274696894be0a10a8e3b7ac3b0d2
Annika Kaiser <ironannebonnie@gmail.com>
2018-07-04 12:38:04 +0200

    minor a11y JavaScript updates for alt texts

--- js/main.js
+++ js/main.js
@@ -141,6 +141,8 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  //adds an image alt text for screen readers which simply notes the //restaurant name - I specifically chose not to add additional text
+  image.alt = restaurant.name;
   li.append(image);
 
   const name = document.createElement('h1');
--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -58,6 +58,8 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  //adds an image alt text for screen readers which simply notes the restaurant name
+  image.alt = restaurant.name;
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
