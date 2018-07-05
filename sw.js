8c50d1f848d72156480d9a6a3e331cd0499f0d91
Annika Kaiser <ironannebonnie@gmail.com>
2018-07-05 19:51:04 +0200

    minor fixes

--- sw.js
+++ sw.js
@@ -29,7 +29,6 @@ self.addEventListener("install", function(event) {
     );
 });
 
-
 self.addEventListener("fetch", function(event) {
     event.respondWith(
         caches.match(event.request).then(function(response) {
