a5406c1303189694c3f4158b60e72e166b0651ed
Annika Kaiser <ironannebonnie@gmail.com>
2018-11-02 19:47:01 +0100

    *

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -26,7 +26,8 @@ class DBHelper {
         //const port = 8000 // Change this to your server port
         //return `http://localhost:${port}/data/restaurants.json`;
         //return data/restaurants.json;
-        return "https://ajk-restaurant-reviews.netlify.com/data/restaurants.json"
+        //return "https://ajk-restaurant-reviews.netlify.com/data/restaurants.json"
+        return `http://localhost:127.0.0.1`;
     }
 
     /**
